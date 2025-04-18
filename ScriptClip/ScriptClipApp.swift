1d319efbf7a4f0cdf05d126b00b268bc9ccbfd79
DavidMaliglowka <13022280+DavidMaliglowka@users.noreply.github.com>
2025-04-18 02:05:22 -0500

    Initial Commit

---
+++ ScriptClip/ScriptClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptClipApp.swift
+//  ScriptClip
+//
+//  Created by David Maliglowka on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ScriptClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScriptClipTests/ScriptClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptClipTests.swift
+//  ScriptClipTests
+//
+//  Created by David Maliglowka on 4/18/25.
+//
+
+import Testing
+@testable import ScriptClip
+
+struct ScriptClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
